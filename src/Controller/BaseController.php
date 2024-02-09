00b50b018b6d4f38023b1fa03ef3e848a4cb2e8b
Erwan DIGNOIRE <dignoire.erwan@gmail.com>
2024-02-09 19:52:12 +0100

    ajout page Ajouter Jeux

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ContactType;
+use App\Form\JeuxType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Contact;
+use App\Entity\Jeux;
+use Doctrine\ORM\EntityManagerInterface;
+
+class BaseController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, EntityManagerInterface $em): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($contact);
+                $em->flush();
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_contact');
+            }
+        }
+        return $this->render('base/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/ajouterJeux', name: 'app_ajouterJeux')]
+    public function ajouterJeux(Request $request, EntityManagerInterface $em): Response
+    {
+        $jeu = new Jeux();
+        $form = $this->createForm(JeuxType::class, $jeu);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($jeu);
+                $em->flush();
+                $this->addFlash('notice','Jeu ajouté');
+                return $this->redirectToRoute('app_ajouterJeux');
+            }
+        }    
+        return $this->render('base/ajouter-jeux.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Entity/Jeux.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuxRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JeuxRepository::class)]
+class Jeux
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $categorie = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $prix = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): static
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
